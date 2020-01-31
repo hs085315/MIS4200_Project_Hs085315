028d83c18a68a78747ae771ab0427d8a7adb6a67
Hannah Sullivan <hs085315@ohio.edu>
2020-01-31 15:31:27 -0500

    Changes to Order Detail

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -10,9 +10,11 @@ namespace MIS4200_Project_Hs085315.Models
         public int orderdetailID { get; set; }
         public int qtyOrdered { get; set; }
         public decimal price { get; set; }
+
         // the next two properties link the orderDetail to the Order
         public int orderID { get; set; }
         public virtual Order Order { get; set; }
+
         // the next two properties link the orderDetail to the Product
         public int productID { get; set; }
         public virtual Product { get; set; }
