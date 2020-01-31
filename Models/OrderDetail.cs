cc78f77c7572acec555555151e8c933fbd82c78b
Hannah Sullivan <hs085315@ohio.edu>
2020-01-31 15:27:24 -0500

    Changes to webconfig file

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Project_Hs085315.Models
+{
+    public class OrderDetail
+    {
+        public int orderdetailID { get; set; }
+        public int qtyOrdered { get; set; }
+        public decimal price { get; set; }
+        // the next two properties link the orderDetail to the Order
+        public int orderID { get; set; }
+        public virtual Order Order { get; set; }
+        // the next two properties link the orderDetail to the Product
+        public int productID { get; set; }
+        public virtual Product { get; set; }
+    }
+}
