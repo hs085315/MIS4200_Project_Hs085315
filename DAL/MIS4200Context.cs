bfc725cf7452abe36c28888e7ca101ac56d4c794
Hannah Sullivan <hs085315@ohio.edu>
2020-02-05 11:08:16 -0500

    Changes in Class Feb 5th 2020

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Project_Hs085315.DAL
+{
+    public class MIS4200Context
+    {
+        public class MIS4200Context : DbContext // inherits from DbContext
+        {
+            public MIS4200Context() : base("name=DefaultConnection")
+            {
+                // this method is a 'constructor' and is called when a new context is created
+                // the base attribute says which connection string to use
+            }
+            // Include each object here. The value inside <> is the name of the class,
+            // the value outside should generally be the plural of the class name
+            // and is the name used to reference the entity in code
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Product> Products { get; set; }
+            public DbSet<OrderDetail> OrderDetails { get; set; }
+        }
+    }
+}
