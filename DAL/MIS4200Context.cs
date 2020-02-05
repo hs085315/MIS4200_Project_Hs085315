0f40321dd8a4fd9a995e2517bef5633dc50dbf84
Hannah Sullivan <hs085315@ohio.edu>
2020-02-05 18:53:38 -0500

    Changes to Context

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -7,7 +7,6 @@ using MIS4200_Project_Hs085315.Models;
 
 namespace MIS4200_Project_Hs085315.DAL
 {
-
         public class MIS4200Context : DbContext // inherits from DbContext
         {
             public MIS4200Context() : base("name=DefaultConnection")
@@ -21,10 +20,10 @@ namespace MIS4200_Project_Hs085315.DAL
             public DbSet<Student> Students { get; set; }
             public DbSet<Course> Courses { get; set; }
             public DbSet<Grade> Grades { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
+            public DbSet<OrderDetail> OrderDetails { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Product> Products { get; set; }
-    }
-    }
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Product> Products { get; set; }
+        }
+}
