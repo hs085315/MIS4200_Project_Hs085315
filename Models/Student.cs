d6f53c2dabd6822c26aaaa76732fa1ae6e57b0de
Hannah Sullivan <hs085315@ohio.edu>
2020-02-19 15:31:26 -0500

    SD3 Changes

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,19 +8,23 @@ namespace MIS4200_Project_Hs085315.Models
 {
     public class Student
     {
+        public System.Guid SID { get; set; }
         public int studentId { get; set; }
+
+        [Display (Name="First name")]
         public string studentFirstName { get; set; }
+
+        [Display (Name="Last name")]
         public string studentLastName { get; set; }
+
+        [Display(Name = "Most used email address")]
         public string email { get; set; }
+
+        [Display(Name = "Mobile phone number")]
         public string phone { get; set; }
+
+        [Display(Name = "When did you join this school?")]
         public DateTime studentSince { get; set; }
-        // add any other fields as appropriate
-        // a customer can have any number of orders, a 1:M relationship,
-        // We represent this in the model with an ICollection
-        // The syntax says we are creating an ICollection of Order objects,
-        // (the name inside the <> is the object name),
-        // and the local name of the collection will be Order
-        // (the object name and the local name do not have to be the same)
         public ICollection<Grade> Grade { get; set; }
 
         public string studentFullName
